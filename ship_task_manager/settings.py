b62d424ee9960612dc4d11ebe6c31164361f6063
phaishuk <xomas@ukr.net>
2023-04-10 17:26:57 +0300

    db connected

--- ship_task_manager/settings.py
+++ ship_task_manager/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = "ship_task_manager.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sailors_db',
+        'USER': 'testadmin',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
