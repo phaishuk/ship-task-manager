d67cfb1ec5ccaa925d447ac11cced0d05851e965
phaishuk <xomas@ukr.net>
2023-04-15 00:53:59 +0300

    fix static files and add a bit margin to css

--- ship_task_manager/settings.py
+++ ship_task_manager/settings.py
@@ -133,7 +133,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATICFILES_DIRS = (BASE_DIR / "static",)
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
