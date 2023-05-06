f0d06f0fa644ab28a75836523dd5e8d5560dcfaa
phaishuk <xomas@ukr.net>
2023-05-06 05:24:08 +0300

    fix allowed hosts

--- ship_task_manager/settings.py
+++ ship_task_manager/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "sailors-app.onrender.com/"]
+ALLOWED_HOSTS = ["127.0.0.1", "sailors-app.onrender.com"]
 
 # Application definition
 
